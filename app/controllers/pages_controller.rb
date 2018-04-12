78d1733aae15e669e1932127329002946fe749b5
donrestarone <shashikejayatunge@gmail.com>
2018-04-12 15:21:02 -0400

    title works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
 	
 	def welcome 
+		@header = "This is the welcome page"
 		render :welcome
 	end 
 
 	def contest
+		@header = "This is the contest page"
 		render :contest
 	end 
+
+
 end
