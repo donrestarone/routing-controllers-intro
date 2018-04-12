14df19b5646683645f006bccb0def322c9b35ad3
donrestarone <shashikejayatunge@gmail.com>
2018-04-12 15:38:08 -0400

    flash hash works for contest end notice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,12 +7,24 @@ class PagesController < ApplicationController
 
 	def contest
 		@header = "This is the contest page"
-		render :contest
+		flash[:notice] = "sorry, the contest has ended"
+		redirect_to '/welcome'
+		#render :contest
+	end 
+
+	def set_kitten_url 
+		requested_size = params[:size]
+		@kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
 	end 
 
 	def kitten 
-		requested_size = params[:size]
-		@kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+		set_kitten_url
 	end
+
+	def kittens 
+		set_kitten_url
+	end 
+
+
 
 end
